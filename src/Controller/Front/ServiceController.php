9fa64a14598b2a6ae45977154f976a028df72727
fxdstudiokg <fxd@studiokg.fr>
2024-08-02 10:49:35 +0200

    Feat pagination

--- src/Controller/Front/ServiceController.php
+++ src/Controller/Front/ServiceController.php
@@ -4,18 +4,24 @@ namespace App\Controller\Front;
 
 use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ServiceController extends AbstractController
 {
-    #[Route('/service', name: 'app_service_show')]
-    public function index(ServiceRepository $serviceRepository): Response
+    #[Route('/service/{page<\d+>?1}', name: 'app_service_show')]
+    public function index(ServiceRepository $serviceRepository, Request $request, int $page = 1): Response
     {
+        $itemsPerPage = 3;
         $services = $serviceRepository->findAllService();
+        $totalPages = ceil(count($services) / $itemsPerPage);
+        $page = max(1, min($page, $totalPages));
      
         return $this->render('front/service/index.html.twig', [
             'services' => $services,
+            'page' => $page,
+            'totalPages' => $totalPages,
         ]);
     }
 }
