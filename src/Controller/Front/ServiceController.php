6ae8ff84f875fd766677a4939002b6852da1baf3
fxdstudiokg <fxd@studiokg.fr>
2024-08-05 07:49:58 +0200

    Feat pagination

--- src/Controller/Front/ServiceController.php
+++ src/Controller/Front/ServiceController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Front;
 
 use App\Repository\ServiceRepository;
+use App\Service\PaginationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,17 +12,17 @@ use Symfony\Component\Routing\Attribute\Route;
 class ServiceController extends AbstractController
 {
     #[Route('/service/{page<\d+>?1}', name: 'app_service_show')]
-    public function index(ServiceRepository $serviceRepository, Request $request, int $page = 1): Response
+    public function index(ServiceRepository $serviceRepository, PaginationService $paginationService,  int $page = 1): Response
     {
         $itemsPerPage = 3;
-        $services = $serviceRepository->findAllService();
-        $totalPages = ceil(count($services) / $itemsPerPage);
-        $page = max(1, min($page, $totalPages));
+        $services = $serviceRepository->findAllService(['deleted_At'=> null]);
+        $paginationData = $paginationService->paginate($services, $page, $itemsPerPage);
      
         return $this->render('front/service/index.html.twig', [
-            'services' => $services,
-            'page' => $page,
-            'totalPages' => $totalPages,
+            'services' => $paginationData['items'],
+            'currentPage' => $paginationData['currentPage'],
+            'totalPages' => $paginationData['totalPages'],
+            'totalItems' => $paginationData['totalItems'],
         ]);
     }
 }
