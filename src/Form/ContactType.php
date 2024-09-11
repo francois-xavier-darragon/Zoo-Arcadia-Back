f87a0b3d6888589bb4ac256f0c75fd437134cd55
fxdstudiokg <fxd@studiokg.fr>
2024-09-11 10:00:54 +0200

    Correction demande de compte

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -14,26 +14,6 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname', TextType::class, [
-                'required' => true,
-                'label' => 'Prénom',
-                'label_attr' => [
-                    'class' => 'col-lg-4 col-form-label required fw-semibold fs-6'
-                ],
-                'attr' => [
-                    'class' => 'form-control form-control-solid',
-                ]
-            ])
-            ->add('lastname', TextType::class, [
-                'required' => true,
-                'label' => 'Nom',
-                'label_attr' => [
-                    'class' => 'col-lg-4 col-form-label required fw-semibold fs-6'
-                ],
-                'attr' => [
-                    'class' => 'form-control form-control-solid',
-                ]
-            ])
             ->add('email', EmailType::class, [
                 'required' => true,
                 'label' => 'Votre adresse email',
